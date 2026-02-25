53438cd62868ea7b9a464b01b6b92298109058cb
Varun <151656164+NVarun21@users.noreply.github.com>
2026-02-25 18:52:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.py
+++ Difficulty: Easy/Array Leaders/array-leaders.py
@@ -1,13 +1,11 @@
 class Solution:
     def leaders(self, arr):
         # code here
-        ans=[]
-        n=len(arr)
-        maxi=arr[n-1]
-        ans.append(maxi)
-        for i in range(n-2,-1,-1):
-            if arr[i]>=maxi:
-                ans.append(arr[i])
-                maxi=arr[i]
-        ans.reverse()
-        return ans
+        leader=[]
+        max_right=arr[len(arr)-1]
+        leader.append(max_right)
+        for i in range(len(arr)-2,-1,-1):
+            if arr[i]>=max_right:
+                max_right=arr[i]
+                leader.append(max_right)
+        return leader[::-1]
