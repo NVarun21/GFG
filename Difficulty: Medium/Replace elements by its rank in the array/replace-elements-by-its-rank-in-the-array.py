109894a1ce0810ba9f7f89e5a8dd50b156412c2e
Varun <151656164+NVarun21@users.noreply.github.com>
2026-03-02 17:36:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Replace elements by its rank in the array/replace-elements-by-its-rank-in-the-array.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    def replaceWithRank(self, N, arr):
+        # Code here
+        sorted_arr=sorted(arr)
+        rank={}
+        current_rank=1
+        for num in sorted_arr:
+            if num not in rank:
+                rank[num]=current_rank
+                current_rank+=1
+        return [rank[num]for num in arr]
